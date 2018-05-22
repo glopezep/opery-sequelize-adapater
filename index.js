17ff6d8eecc5e1746b97f64930c7f098d34939dd
Guillermo Lopez <guillermolopez2529@gmail.com>
2018-05-21 21:46:13 -0400

    Remove unnecessary files

--- index.js
+++ index.js
@@ -2,11 +2,9 @@
 
 const setupSequelize = require('./lib/sequelize')
 const getModels = require('./lib/getModels')
-const baseService = require('./lib/baseService')
 
 module.exports = {
   name: 'operySequelizeAdapater',
   setupOrm: setupSequelize,
   getModels,
-  baseService
 }
